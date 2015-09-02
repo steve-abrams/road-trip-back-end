6c112600539c2abeb38945d1f7fa652e5de08d41
Roberto Ortega <berto.ort@gmail.com>
2015-09-02 13:43:02 -0600

    devise users

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'trips#index'
 
   # Example of regular route:
-    get 'trips/new' => 'trips#new'
+  get 'trips/new' => 'trips#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
