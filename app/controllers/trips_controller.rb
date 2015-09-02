268cc78ea917708687f11158cfab47915cef83bf
Roberto Ortega <berto.ort@gmail.com>
2015-09-02 13:57:06 -0600

    trip 7 routes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -21,6 +21,27 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if Trip.find(params[:id]).destroy
+      redirect_to trips_path
+    else
+      render :show
+    end
+  end
+
   def trip_params
     params.require(:trip).permit(:name, :start_location, :end_location, :start_date, :end_date)
   end
