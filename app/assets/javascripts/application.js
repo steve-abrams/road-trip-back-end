99fe96538f2ef7da18648b09076f2f26ebdbbec3
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-09-03 14:54:11 -0600

    Display map on user show page, will move to trip show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,11 @@
 //= require foundation
 
 $(function(){ $(document).foundation(); });
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
---
+++ app/assets/javascripts/components/map.js
@@ -0,0 +1,14 @@
+// 
+//
+// var map = React.createClass({
+//   render: function () {
+//     var map
+//     function initMap() {
+//       map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -34.397, lng: 150.644},
+//         zoom: 8
+//       });
+//     }
+//     return (<div id="map"></div>)
+//   }
+// })
