4975599716d7831bfbc94712a889281bcbd1e7f2
Dylan Zincone <dylan.zincone@gmail.com>
2015-09-08 23:05:11 -0600

    Updated login redirects for devise in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def after_sign_in_path_for(resource)
+  user_path(User.find(current_user).id)
+  end
+  def after_sign_up_path_for(resource)
+  user_path(User.find(current_user).id)
+  end
   protect_from_forgery with: :exception
 end
