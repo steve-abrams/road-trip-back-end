d8219404dd26e30bf81ec66bc443bc65c551c6d4
Dylan Zincone <dylan.zincone@gmail.com>
2015-09-09 21:14:01 -0600

    Updated link to trip pages, need to fix the maps error

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,12 @@ ActiveRecord::Schema.define(version: 20150903221107) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string  "name"
-    t.date    "date"
-    t.string  "description"
-    t.integer "trip_id"
+    t.string   "name"
+    t.date     "date"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "trip_id"
   end
 
   create_table "trips", force: :cascade do |t|
