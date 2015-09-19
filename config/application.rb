af46147208e69f6b065b561bf104fd7884cb694d
Nick Mattei <nmattei89@gmail.com>
2015-09-18 18:30:27 -0600

    trip button moved

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ GOOGLEAPIKEY = ENV['GOOGLEAPI']
 module RoadTripBackEnd
   class Application < Rails::Application
 
-
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
