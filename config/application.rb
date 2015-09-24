8933e54f0e6217f502f4a3251059a31e6000e164
Dylan Zincone <dylan.zincone@gmail.com>
2015-09-24 15:27:08 -0600

    dylan best

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
-GOOGLEAPIKEY = ENV['GOOGLEAPI']
+# GOOGLEAPIKEY = ENV['GOOGLEAPI']
 
 
 module RoadTripBackEnd
